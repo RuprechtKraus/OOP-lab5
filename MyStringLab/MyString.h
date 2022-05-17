efa3d13c5ff760371b9864a03a0d4c807a9e614d
Ruprecht Kraus <zombiegooood@gmail.com>
2022-05-17 16:22:30 +0300

    Revert "Non-constant iterators"
    This reverts commit 89258f213912e048c1625bc876f6927bfac08407.

--- MyStringLab/MyString.h
+++ MyStringLab/MyString.h
@@ -1,41 +1,10 @@
 #pragma once
-#include <stdexcept>
 #include <string>
-
-class MyStringIterator
-{
-public:
-	using iterator_category = std::random_access_iterator_tag;
-    using difference_type   = std::ptrdiff_t;
-    using value_type        = char;
-    using pointer           = value_type*; 
-    using reference         = value_type&; 
-
-	MyStringIterator(pointer ptr);
-
-	MyStringIterator& operator++();
-	MyStringIterator operator++(int);
-	MyStringIterator& operator--();
-	MyStringIterator operator--(int);
-	MyStringIterator& operator+=(int offset);
-	friend const MyStringIterator operator+(const MyStringIterator& iter, int offset);
-	friend const MyStringIterator operator+(int offset, const MyStringIterator& iter);
-	reference operator[](int index) const;
-	reference operator*() const;
-	pointer operator->();
-
-	friend bool operator==(const MyStringIterator& left, const MyStringIterator& right);
-	friend bool operator!=(const MyStringIterator& left, const MyStringIterator& right);
-
-private:
-	pointer m_ptr;
-};
+#include <stdexcept>
 
 class MyString
 {
 public:
-	using Iterator = MyStringIterator;
-
 	MyString() noexcept;
 	MyString(const char* pStr);
 	MyString(const char* pStr, size_t length);
@@ -61,16 +30,13 @@ public:
 	const char& operator[](size_t index) const;
 	char& operator[](size_t index);
 
-	static int Compare(const MyString& left, const MyString& right) noexcept;
-	static void Swap(MyString& left, MyString& right) noexcept;
-
 	size_t GetLength() const noexcept;
 	const char* GetStringData() const noexcept;
 	MyString SubString(size_t start, size_t length = SIZE_MAX) const;
 	void Clear() noexcept;
 
-	Iterator begin() const;
-	Iterator end() const;
+	static int Compare(const MyString& left, const MyString& right) noexcept;
+	static void Swap(MyString& left, MyString& right) noexcept;
 
 private:
 	void SetEmpty() noexcept;
--- MyStringLab/MyStringLab.cpp
+++ MyStringLab/MyStringLab.cpp
@@ -1,18 +1,6 @@
 ﻿#include <iostream>
-#include <string>
-#include "MyString.h"
 
 int main()
 {
-	/*std::string str("Hello");
-	auto a{ str.begin() + 1 };
-	auto b { a + 2 };
-
-	b - a;*/
-
-	MyString myString("Hello");
-	auto a{ myString.begin() };
-	std::cout << *(++(++a));
-
 	return 0;
 }
